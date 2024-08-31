cad2b2d45622dcd38dc0dfa61d6bfb75b374820a
Aboyeji-Isaac <141873116+Aboyeji-Isaac@users.noreply.github.com>
2024-08-31 03:52:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const ctx = document.getElementById('myDonutChart').getContext('2d');
+const myDonutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Bluevet'],
+        datasets: [{
+            data: [300, 50, 100],
+            backgroundColor: ['#6692fd', '#5e58f6', '#34308f', '#55af7'],
+            borderColor: 'transparent',
+            // hoverBackgroundColor: ['#ff6384', '#36a2eb', '#ffcd56', '#5e5af7']
+            // borderWidth:1,
+            hoverOffset: 50,
+        }]
+    },
+    options: {
+        responsive: true,
+        cutout: '80%', // Size of the donut hole
+        animation:{
+            duration: 500,
+            easing: 'easeOutBounce',
+            animateScale: true,
+            // animateRotate: true,
+        },
+
+        hover:{
+            onHover: function(e, activeElements){
+                if(activeElements.length){
+                    e.target.style.cursor = 'pointer';
+                } else{
+                    e.terget.style.cursor = 'default';
+                }
+            },
+            mode: 'nearest',
+            intersect: true,
+        },
+        plugins: {
+            legend: {
+                position: 'top', // Position of the legend
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        return `${context.label}: ${context.raw}`;
+                    }
+                }
+            }
+        }
+        
+    }
+});
